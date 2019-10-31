ae18f607583b33252c8db9119e40cf0420617a55
Pav Sidhu <pav@pavsidhu.com>
2019-10-31 17:12:39 +0000

    Add basic tab setup

---
+++ app/src/Matches.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Matches() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Matches Screen</Text>
+    </View>
+  )
+}
