3d9b74e988c92ab5d48f2e7372dc48d409d1c2a2
Pav Sidhu <pav@pavsidhu.com>
2019-10-31 18:48:51 +0000

    Setup chat

--- app/src/Chat.tsx
+++ app/src/Chat.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { GiftedChat } from 'react-native-gifted-chat'
+import styled from 'styled-components/native'
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background: #fefefe;
+  margin-bottom: 18px;
+`
+
+const MessageInput = styled.TextInput`
+  padding: 16px;
+  background: #fefefe;
+  font-size: 16px;
+  border-color: #eeeeee;
+  border-top-width: 2px;
+`
 
 export default function Chat() {
+  const messages = []
+
+  function onSend(messages) {}
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Chat Screen</Text>
-    </View>
+    <Container>
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{ _id: 1 }}
+        renderInputToolbar={props => <MessageInput {...props} />}
+      />
+    </Container>
   )
 }
