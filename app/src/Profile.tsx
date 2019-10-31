e6310268a4eb0029aa5f0e8063bf0699865e8686
Pav Sidhu <pav@pavsidhu.com>
2019-10-31 19:12:43 +0000

    Add Profile tab

--- app/src/Profile.tsx
+++ app/src/Profile.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import styled from 'styled-components/native'
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background: #fefefe;
+`
+
+const Section = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+
+const ProfilePicture = styled.Image`
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
+  margin-bottom: 24px;
+`
+
+const Name = styled.Text`
+  font-size: 24px;
+`
 
 export default function Profile() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
-    </View>
+    <Container>
+      <Section>
+        <ProfilePicture source={{ uri: undefined }} />
+        <Name></Name>
+      </Section>
+
+      <Section></Section>
+    </Container>
   )
 }
