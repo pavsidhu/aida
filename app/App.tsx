6a353989ea431f24e2f40c49e1037a3d0de2096a
Pav Sidhu <pav@pavsidhu.com>
2019-10-31 17:33:49 +0000

    Setup Navigation

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,40 @@
 import { createAppContainer } from 'react-navigation'
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { createStackNavigator } from 'react-navigation-stack'
 import Chat from './src/Chat'
 import Matches from './src/Matches'
 import Profile from './src/Profile'
 
-const App = createMaterialBottomTabNavigator({
-  Chat: { screen: Chat },
-  Matches: { screen: Matches },
-  Profile: { screen: Profile }
+const ChatStack = createStackNavigator({
+  Chat: {
+    screen: Chat,
+    navigationOptions: { header: null }
+  }
 })
+const MatchesStack = createStackNavigator({
+  Matches: {
+    screen: Matches,
+    navigationOptions: { header: null }
+  }
+})
+const ProfileStack = createStackNavigator({
+  Profile: {
+    screen: Profile
+  }
+})
+
+const App = createMaterialBottomTabNavigator(
+  {
+    Chat: { screen: ChatStack },
+    Matches: { screen: MatchesStack },
+    Profile: { screen: ProfileStack }
+  },
+  {
+    barStyle: {
+      backgroundColor: '#fefefe'
+    }
+  }
+)
 
 export default createAppContainer(App)
