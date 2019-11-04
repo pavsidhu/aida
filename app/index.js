977d8d273d49aa29bcbc0835e4e392fc9449841f
Pav Sidhu <pav@pavsidhu.com>
2019-11-04 13:05:43 +0000

    Ignore library warnings

--- app/index.js
+++ app/index.js
@@ -1,5 +1,10 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+import { YellowBox } from 'react-native'
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillReceiveProps has been renamed'
+])
 
 AppRegistry.registerComponent(appName, () => App)
