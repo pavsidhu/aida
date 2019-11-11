e99d930fd07ff1bcd98a01b33fc4a3513d0a7273
Pav Sidhu <pav@pavsidhu.com>
2019-11-11 17:38:25 +0000

    Setup firebase admin

--- server/app.py
+++ server/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+
+import firebase_admin
+from firebase_admin import firestore
+from flask import Flask, request
 
 app = Flask(__name__)
+
+firebase_admin.initialize_app()
+db = firestore.client()
 
 
 @app.route("/status")
