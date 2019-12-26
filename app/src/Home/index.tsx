9587028f59b21b589741ecfc463d1a9976d2ffca
Pav Sidhu <pav@pavsidhu.com>
2019-12-26 22:46:17 +0000

    Fix Home component header navigation options

--- app/src/Home/index.tsx
+++ app/src/Home/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
-import {
-  createStackNavigator,
-  NavigationStackScreenComponent
-} from 'react-navigation-stack'
+import { createStackNavigator } from 'react-navigation-stack'
 import { TouchableRipple } from 'react-native-paper'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import Chat from './Chat'
@@ -19,58 +16,65 @@ const DEFAULT_NAVIGATION_OPTIONS = {
   }
 }
 
-interface Screen {
-  name: string
-  screen: (props: any) => JSX.Element | null
-  headerTitle?: boolean
-  navigationOptions?: NavigationStackScreenComponent['navigationOptions']
-}
-
-const generateTab = (screens: Screen[]) =>
-  createStackNavigator(
-    screens.reduce(
-      (acc, screen) => ({
-        ...acc,
-        [screen.name]: {
-          screen: screen.screen,
-          navigationOptions: {
-            ...DEFAULT_NAVIGATION_OPTIONS,
-            ...screen.navigationOptions,
-            headerTitle: screen.headerTitle !== false ? screen.name : null
-          }
-        }
-      }),
-      {}
-    ),
-    { headerLayoutPreset: 'center' }
-  )
-
-const ChatTab = generateTab([{ name: 'Chat', screen: Chat }])
-
-const MatchesTab = generateTab([
-  { name: 'Matches', screen: Matches },
-  { name: 'MatchChat', screen: MatchChat, headerTitle: false }
-])
-
-const ProfileTab = generateTab([
+const ChatTab = createStackNavigator(
   {
-    name: 'Profile',
-    screen: Profile,
-    navigationOptions: ({ navigation }) => ({
-      headerRight: () => (
-        <TouchableRipple
-          onPress={() => {
-            navigation.navigate('Settings')
-          }}
-          style={{ padding: 16 }}
-        >
-          <MaterialIcon name="settings" size={24} color="#1b1b1b" />
-        </TouchableRipple>
-      )
-    })
+    Chat: {
+      screen: Chat,
+      navigationOptions: {
+        ...DEFAULT_NAVIGATION_OPTIONS,
+        headerTitle: 'Chat'
+      }
+    }
   },
-  { name: 'Settings', screen: Settings }
-])
+  { headerLayoutPreset: 'center' }
+)
+
+const MatchesTab = createStackNavigator(
+  {
+    Matches: {
+      screen: Matches,
+      navigationOptions: {
+        ...DEFAULT_NAVIGATION_OPTIONS,
+        headerTitle: 'Matches'
+      }
+    },
+    MatchChat: {
+      screen: MatchChat,
+      navigationOptions: DEFAULT_NAVIGATION_OPTIONS
+    }
+  },
+  { headerLayoutPreset: 'center' }
+)
+
+const ProfileTab = createStackNavigator(
+  {
+    Profile: {
+      screen: Profile,
+      navigationOptions: ({ navigation }) => ({
+        ...DEFAULT_NAVIGATION_OPTIONS,
+        headerTitle: 'Profile',
+        headerRight: () => (
+          <TouchableRipple
+            onPress={() => {
+              navigation.navigate('Settings')
+            }}
+            style={{ padding: 16 }}
+          >
+            <MaterialIcon name="settings" size={24} color="#1b1b1b" />
+          </TouchableRipple>
+        )
+      })
+    },
+    Settings: {
+      screen: Settings,
+      navigationOptions: {
+        ...DEFAULT_NAVIGATION_OPTIONS,
+        headerTitle: 'Settings'
+      }
+    }
+  },
+  { headerLayoutPreset: 'center' }
+)
 
 const generateTabBarIcon = (name: string, focused: boolean) => (
   <MaterialIcon name={name} size={24} color={focused ? '#705EF1' : '#9b9b9b'} />
