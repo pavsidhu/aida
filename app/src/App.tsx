04aa25507e609ce33d5c39c28f0c957be77bec2a
Pav Sidhu <pav@pavsidhu.com>
2020-01-03 19:22:28 +0000

    Fetch location on each app launch

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-community/async-storage'
+import Geolocation from 'react-native-geolocation-service'
 import {
   createAppContainer,
   createSwitchNavigator,
   NavigationContainerProps
 } from 'react-navigation'
 import auth from '@react-native-firebase/auth'
+import firestore from '@react-native-firebase/firestore'
 import { create } from 'mobx-persist'
 
 import Home from './Home'
 import SignIn from './SignIn'
 import onboardingStore from './onboarding/onboardingStore'
-import { StatusBar } from 'react-native'
+import { StatusBar, Alert } from 'react-native'
 import colors from './colors'
 import './questioning/scheduler'
 
@@ -25,6 +27,7 @@ const Navigator = createSwitchNavigator({
 function App(props: NavigationContainerProps) {
   const [initialised, setInitialised] = useState(false)
   const [hydrated, setHydrated] = useState(false)
+  const { currentUser } = auth()
 
   useEffect(() => {
     hydrate('onboarding', onboardingStore).then(() => setHydrated(true))
@@ -38,6 +41,30 @@ function App(props: NavigationContainerProps) {
       }),
     []
   )
+
+  // Update user's location everytime the app is launched
+  useEffect(() => {
+    if (!currentUser) return
+
+    Geolocation.getCurrentPosition(
+      position =>
+        firestore()
+          .collection('users')
+          .doc(currentUser.uid)
+          .update({
+            location: new firestore.GeoPoint(
+              position.coords.latitude,
+              position.coords.longitude
+            )
+          }),
+      () =>
+        Alert.alert(
+          'Location Access',
+          'Please enable location permissions to use Aida'
+        ),
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    )
+  }, [currentUser])
 
   return (
     initialised &&
