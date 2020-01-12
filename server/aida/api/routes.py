6402f650a7f7611d7ce00059747628567e9be3ab
Pav Sidhu <pav@pavsidhu.com>
2020-01-12 14:50:38 +0000

    Add question sending scheduler

--- server/aida/api/routes.py
+++ server/aida/api/routes.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from flask import Blueprint
 
 from aida.api.decorators import auth_required
+from aida.scheduler.questions import start_question_scheduler
 
 blueprint = Blueprint("routes", __name__, url_prefix="/")
 
@@ -14,6 +15,7 @@ blueprint = Blueprint("routes", __name__, url_prefix="/")
 @auth_required
 def start_questions(user_id):
     """Start scheduling questions to ask the user"""
+    start_question_scheduler(user_id)
     return {}, 200
 
 
---
+++ server/aida/scheduler/questions.py
@@ -0,0 +1,99 @@
+import re
+import random
+from datetime import datetime, timedelta
+
+import firebase_admin
+from firebase_admin import firestore
+from redis import Redis
+from rq import Queue
+from rq_scheduler import Scheduler
+
+from dotenv import load_dotenv
+
+redis = Redis()
+scheduler = Scheduler(connection=redis)
+
+MINIMUM_QUESTION_TIME = 30
+QUESTION_TIME_RANGE = 300
+
+
+def start_question_scheduler(user_id):
+    """Schedules sending a question to the user"""
+    minutes = random.randint(MIN_QUESTION_TIME, MIN_QUESTION_TIME + QUESTION_TIME_RANGE)
+    scheduler.enqueue_in(timedelta(seconds=10), send_question, user_id)
+
+
+def send_question(user_id):
+    load_dotenv()
+    firebase_admin.initialize_app()
+    db = firestore.client()
+
+    # Get a question from Firestore
+    question = fetch_random_question(db, user_id)
+
+    if question:
+        # Add question to messages with Aida
+        add_question_to_messages(db, user_id, question)
+
+        # Schedule the next question
+        start_question_scheduler(user_id)
+
+
+def fetch_random_question(db, user_id):
+    """Fetches a random question for Aida to ask"""
+
+    # Fetch all the questions already been asked to the user
+    asked_questions_ids = [
+        doc.to_dict()["question_id"]
+        for doc in db.collection("asked_questions")
+        .where("user_id", "==", user_id)
+        .stream()
+    ]
+
+    # Fetch all the questions
+    questions = [
+        {**doc.to_dict(), **{"id": doc.id}}
+        for doc in db.collection("questions").stream()
+    ]
+
+    for question in questions:
+        # Check if the question has been asked
+        if question["id"] in asked_questions_ids:
+            continue
+
+        # Record the question as being asked
+        db.collection("asked_questions").add(
+            {"user_id": user_id, "question_id": question["id"]}
+        )
+
+        return question["question"]
+
+    # Handle the unlikely case where the user has ran out of questions
+    return None
+
+
+def add_question_to_messages(db, user_id, question):
+    # Get user
+    user = db.collection("users").document(user_id).get().to_dict()
+
+    """Add the question to the user's messages"""
+    db.collection("users").document(user_id).collection("messages").add(
+        {
+            "content": parseMessage(question, user),
+            "type": "text",
+            "sender": None,
+            "createdAt": datetime.now(),
+        }
+    )
+
+
+def parseMessage(message, user):
+    tags = re.findall("{{(.*?)}}", message)
+
+    for tag in tags:
+        data = user.get(tag)
+
+        if data:
+            message.replace("{{tag}}", data)
+
+    return message
