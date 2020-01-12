d0f71ae2a695fe15c21e376a9f0ee49c4448886f
Pav Sidhu <pav@pavsidhu.com>
2020-01-12 14:46:19 +0000

    Fix location not always correct

--- app/src/util/getCity.ts
+++ app/src/util/getCity.ts
@@ -8,13 +8,9 @@ export default async function getCity(latitude: number, longitude: number) {
 
   const { address_components } = response.results[0]
 
-  const locality = address_components.find(component =>
-    component.types.includes('locality')
+  const town = address_components.find(component =>
+    component.types.includes('postal_town')
   ).long_name
 
-  const level2Area = address_components.find(component =>
-    component.types.includes('administrative_area_level_2')
-  ).long_name
-
-  return `${locality}, ${level2Area}`
+  return town
 }
