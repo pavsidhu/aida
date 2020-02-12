6b9309a34e91a3fcfea64acc2d05d0782c5bd833
Pav Sidhu <pav@pavsidhu.com>
2020-02-12 21:40:36 +0000

    Add splash screen

--- app/android/app/src/main/java/com/matcher/MainActivity.java
+++ app/android/app/src/main/java/com/matcher/MainActivity.java
@@ -1,15 +1,25 @@
 package com.matcher;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "Matcher";
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Matcher";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, true);
+        super.onCreate(savedInstanceState);
+    }
 }
