304f7ff9a8ab08e79a2af802e1e575e0b462d41b
Pav Sidhu <pav@pavsidhu.com>
2020-02-26 13:15:22 +0000

    Add demo routes

--- server/aida/api/routes.py
+++ server/aida/api/routes.py
@@ -6,9 +6,9 @@ from datetime import datetime
 from flask import Blueprint
 
 from aida.api.decorators import auth_required
-from aida.scheduler.questions import start_question_scheduler
-from aida.scheduler.matching import start_matching_scheduler
-from aida.scheduler.user_analysis import start_user_analysis_scheduler
+from aida.scheduler.questions import start_question_scheduler, send_question
+from aida.scheduler.matching import start_matching_scheduler, find_match
+from aida.scheduler.user_analysis import start_user_analysis_scheduler, user_analysis
 
 blueprint = Blueprint("routes", __name__, url_prefix="/")
 
@@ -34,4 +34,25 @@ def start_user_analysis(user_id):
 def start_matching(user_id):
     """Start scheduling questions to ask the user"""
     start_matching_scheduler(user_id)
+    return {}, 200
+
+
+@blueprint.route("/questions/demo/<string:user_id>", methods=["GET"])
+def demo_questions(user_id):
+    """Start scheduling questions to ask the user"""
+    send_question(user_id)
+    return {}, 200
+
+
+@blueprint.route("/user_analysis/demo/<string:user_id>", methods=["GET"])
+def demo_user_analysis(user_id):
+    """Start user analysis"""
+    find_match(user_id)
+    return {}, 200
+
+
+@blueprint.route("/matching/demo/<string:user_id>", methods=["GET"])
+def demo_matching(user_id):
+    """Start scheduling questions to ask the user"""
+    user_analysis(user_id)
     return {}, 200
