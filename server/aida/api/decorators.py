988942ce807b1a565624bb911ea6dcba9c5ae810
Pav Sidhu <pav@pavsidhu.com>
2020-02-26 13:15:02 +0000

    Remove test code in server auth decorator

--- server/aida/api/decorators.py
+++ server/aida/api/decorators.py
@@ -9,8 +9,6 @@ def auth_required(function):
 
     @wraps(function)
     def wrapper(*args, **kwargs):
-        return function("Npu2xs1KiqVtQvGlKEBZ6iSLO0C3", *args, **kwargs)
-
         authorization = request.headers.get("Authorization")
 
         if not authorization:
