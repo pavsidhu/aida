e95d3fec7fb837ced364e5e05203c75cf292a0dc
Pav Sidhu <pav@pavsidhu.com>
2020-03-05 21:03:04 +0000

    Change get city to better function

--- app/src/util/getCity.ts
+++ app/src/util/getCity.ts
@@ -3,14 +3,30 @@ import config from '../../config'
 
 Geocoder.init(config.googleMaps.apiKey)
 
+const intersect = (a, b) => new Set(a.filter(v => ~b.indexOf(v)))
+
+const gettowncity = function(addressComponents) {
+  if (
+    typeof addressComponents === 'object' &&
+    addressComponents instanceof Array
+  ) {
+    const order = [
+      'sublocality_level_1',
+      'neighborhood',
+      'locality',
+      'postal_town'
+    ]
+
+    for (let component of addressComponents) {
+      if (intersect(order, component.types).size > 0) return component
+    }
+  }
+  return false
+}
+
 export default async function getCity(latitude: number, longitude: number) {
   const response = await Geocoder.from(latitude, longitude)
 
   const { address_components } = response.results[0]
-
-  const town = address_components.find(component =>
-    component.types.includes('postal_town')
-  ).long_name
-
-  return town
+  return gettowncity(address_components).long_name
 }
