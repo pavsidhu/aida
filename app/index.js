d375feb5127918a05c369a9484436684b6405cd5
Pav Sidhu <pav@pavsidhu.com>
2020-04-05 18:29:02 +0100

    Ignore yellowbox warnings

--- app/index.js
+++ app/index.js
@@ -1,11 +1,7 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
-import { YellowBox } from 'react-native'
 
-YellowBox.ignoreWarnings([
-  'Warning: componentWillReceiveProps has been renamed',
-  'Error: [messaging/unregistered]'
-])
+console.disableYellowBox = true
 
 AppRegistry.registerComponent(appName, () => App)
