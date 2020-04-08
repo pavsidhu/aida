e29ba6fbdb523e92215d11c88bf296a1520ffceb
Pav Sidhu <pav@pavsidhu.com>
2020-04-08 20:36:47 +0100

    Update traits in profile

--- app/src/Home/Profile/Trait.tsx
+++ app/src/Home/Profile/Trait.tsx
@@ -11,67 +11,29 @@ const Container = styled.View`
   border-bottom-color: ${colors.lightGrey};
 `
 
+const Top = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
+`
+
+const Value = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: ${colors.white};
+  text-align: center;
+  line-height: 32px;
+  padding: 8px;
+  border-radius: 14px;
+  background-color: ${colors.purple};
+`
+
 const Title = styled.Text`
   font-size: 18px;
   font-weight: bold;
   color: ${colors.black};
   text-transform: capitalize;
-  text-align: center;
-  margin-bottom: 16px;
-`
-
-const BarContainer = styled.View`
-  flex-direction: row;
-  width: 100%;
-  align-items: center;
-`
-
-const BarLowValue = styled.Text`
-  margin-right: 8px;
-  font-size: 14px;
-  font-weight: bold;
-
-  ${(props: { shouldHighlight: boolean }) =>
-    props.shouldHighlight &&
-    css`
-      color: ${colors.purple};
-    `}
-`
-
-const BarHighValue = styled.Text`
-  margin-left: 8px;
-  font-size: 14px;
-  font-weight: bold;
-
-  ${(props: { shouldHighlight: boolean }) =>
-    props.shouldHighlight &&
-    css`
-      color: ${colors.purple};
-    `}
-`
-
-const BarNames = styled.View`
-  margin-top: 4px;
-  flex-direction: row;
-  justify-content: space-between;
-`
-
-const BarName = styled.Text`
-  font-size: 14px;
-  font-weight: bold;
-  text-transform: capitalize;
-
-  ${(props: { shouldHighlight: boolean }) =>
-    props.shouldHighlight &&
-    css`
-      color: ${colors.purple};
-    `}
-`
-
-const Bar = styled(LinearGradient)`
-  flex: 1;
-  height: 12px;
-  border-radius: 6px;
 `
 
 const Description = styled.Text`
@@ -89,50 +51,20 @@ function percentAsString(value: number) {
   return (value * 100).toFixed(0) + '%'
 }
 
-function generateLocation(percent: number) {
-  if (percent >= 0.8) return [0, 0.8, 0.8, 1]
-  if (percent <= 0.2) return [0, 0.2, 0.2, 1]
-  return [0, percent, percent, 1]
-}
-
 export default function Trait(props: Props) {
   const type = props.type as keyof typeof personalities
   const personality = personalities[type]
 
   // If higher in a trait
-  const isHigh = props.value > 0.5
-
-  const lowPercent = percentAsString(1 - props.value)
-  const highPercent = percentAsString(props.value)
+  const isHigh = props.value >= 0.5
   const { description } = isHigh ? personality.high : personality.low
-
-  const location = generateLocation(props.value)
 
   return (
     <Container>
-      <Title>{type}</Title>
-      <BarContainer>
-        <BarLowValue shouldHighlight={!isHigh}>{lowPercent}</BarLowValue>
-        <Bar
-          colors={[
-            colors.purple,
-            colors.purple,
-            colors.lightGrey,
-            colors.lightGrey
-          ]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 0 }}
-          useAngle={true}
-          angle={isHigh ? -90 : 90}
-          locations={location}
-        />
-        <BarHighValue shouldHighlight={isHigh}>{highPercent}</BarHighValue>
-      </BarContainer>
-
-      <BarNames>
-        <BarName shouldHighlight={!isHigh}>{personality.low.title}</BarName>
-        <BarName shouldHighlight={isHigh}>{personality.high.title}</BarName>
-      </BarNames>
+      <Top>
+        <Title>{type}</Title>
+        <Value>{percentAsString(props.value)}</Value>
+      </Top>
 
       {description.map((description, index) => (
         <Description key={index}>{'\u2022  ' + description}</Description>
